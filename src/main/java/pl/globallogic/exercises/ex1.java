792c97afb50c652dc236f9c067cb4a279cad7cba
ShiroTsuma-github <spam.shiro@gmail.com>
2023-08-02 13:39:01 +0200

    First exercises

---
+++ src/main/java/pl/globallogic/exercises/ex1.java
@@ -0,0 +1,18 @@
+package pl.globallogic.exercises;
+
+
+public class ex1 {
+    public static void checkNumber(int number)
+    {
+    if(number > 0) System.out.println("positive");
+    else if(number < 0) System.out.println("negative");
+    else System.out.println("zero");
+    }
+
+    public static void main(String[] args)
+    {
+        checkNumber(5);
+        checkNumber(0);
+        checkNumber(-5);
+    }
+}
